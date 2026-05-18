cd842bc1d7d0475114e08c2c058b37e90a7bcbb5
Pich-dara <hengpichdara6666@gmail.com>
2026-05-19 04:05:37 +0700

    Fix syntax space error in api path and update public index

--- public/index.php
+++ public/index.php
@@ -46,6 +46,9 @@ require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
+// 🔥 បន្ថែមកូដ ១ បន្ទាត់នេះចូល ដើម្បីដោះស្រាយរឿង Read-only សហការជាមួយ Environment Variables
+$app->useStoragePath('/tmp/storage');
+
 $kernel = $app->make(Kernel::class);
 
 $response = $kernel->handle(
